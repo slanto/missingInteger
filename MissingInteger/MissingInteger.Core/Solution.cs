bf59e24bbf7f0bbdd84ee104c297982f6189042c
slanto <slawek.antos@outlook.com>
2017-01-13 16:29:33 +0100

    Setup solution

---
+++ MissingInteger/MissingInteger.Core/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace MissingInteger.Core
+{
+    public class Solution
+    {
+        public int GetMissingInteger(int[] array)
+        {
+            return 5;
+        }
+    }
+}
---
+++ MissingInteger/MissingInteger.Tests/SolutionTests.cs
@@ -0,0 +1,18 @@
+﻿namespace MissingInteger.Tests
+{
+    using MissingInteger.Core;
+
+    using Xunit;
+
+    public class SolutionTests
+    {
+        [Theory]
+        [InlineData(new [] { 1, 3, 6, 4, 1, 2 }, 5)]
+        public void GetMissingInteger_ReturnsMissingIntegerFromGivenArray(int[] array, int expected)
+        {
+            var sut = new Solution();
+            var actual = sut.GetMissingInteger(array);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
