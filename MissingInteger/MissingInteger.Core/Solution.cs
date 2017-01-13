6c3b38627c6db439b299ffdbf4cd3345010478c7
slanto <slawek.antos@outlook.com>
2017-01-13 16:49:43 +0100

    Solution implemented

--- MissingInteger/MissingInteger.Core/Solution.cs
+++ MissingInteger/MissingInteger.Core/Solution.cs
@@ -1,10 +1,21 @@
 ﻿namespace MissingInteger.Core
 {
+    using System;
+    using System.Linq;
+
     public class Solution
     {
         public int GetMissingInteger(int[] array)
         {
-            return 5;
+            var min = 1;
+            foreach (var elem in array.Where(e => e > 0).OrderBy(e => e))
+            {
+                if (min == elem)
+                {
+                    min++;
+                }
+            }
+            return min;
         }
     }
 }
--- MissingInteger/MissingInteger.Tests/SolutionTests.cs
+++ MissingInteger/MissingInteger.Tests/SolutionTests.cs
@@ -8,6 +8,7 @@
     {
         [Theory]
         [InlineData(new [] { 1, 3, 6, 4, 1, 2 }, 5)]
+        [InlineData(new[] { int.MinValue, 0, 2, 4, 3, int.MinValue }, 1)]        
         public void GetMissingInteger_ReturnsMissingIntegerFromGivenArray(int[] array, int expected)
         {
             var sut = new Solution();
